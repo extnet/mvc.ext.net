5bc29553cbe35b6af1e6542d815fd610e4caa419
Geoffrey McGill <geoff@object.net>
2016-02-16 15:06:30 -0700

    Update to pull correct README.md and CHANGELOG.md documents

--- src/Areas/Getting_Started/Controllers/READMEController.cs
+++ src/Areas/Getting_Started/Controllers/READMEController.cs
@@ -3,11 +3,11 @@ using System.Web.Mvc;
 
 namespace Ext.Net.MVC.Examples.Areas.Getting_Started.Controllers
 {
-    public class BREAKING_CHANGESController : Controller
+    public class READMEController : Controller
     {
         public ActionResult Index()
         {
-            StreamReader streamReader = System.IO.File.OpenText(Server.MapPath("~/App_Readme/Ext.NET/BREAKING_CHANGES.txt"));
+            StreamReader streamReader = System.IO.File.OpenText(Server.MapPath("~/App_Readme/Ext.NET/README.md"));
             return View("Index", (object)streamReader.ReadToEnd());
         }
     }
