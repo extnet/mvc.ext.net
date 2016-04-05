eaaf2e21ee06d3c3b2e91fbf0c0c48c7a9a94a5d
Geoffrey McGill <geoff@object.net>
2016-04-05 09:53:43 -0600

    Updated to official Ext.NET MVC 4.0.0 release

--- src/App_Start/ExtNetConfig.cs
+++ src/App_Start/ExtNetConfig.cs
@@ -1,17 +1,17 @@
 using System.Web.Mvc;
 using System.Web.Routing;
-
+ 
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Ext.Net.MVC.Examples.ExtNetConfig), "Start")]
 
-namespace Ext.Net.MVC.Examples
+namespace Ext.Net.MVC.Examples 
 {
-    public static class ExtNetConfig
+    public static class ExtNetConfig 
     {
-        public static void Start()
+        public static void Start() 
         {
             ExtNetConfig.RegisterRoutes(RouteTable.Routes);
         }
-
+ 
         public static void RegisterRoutes(RouteCollection routes)
         {
             // Ignore all ext.axd embedded resource paths
@@ -25,8 +25,8 @@ namespace Ext.Net.MVC.Examples
             );
         }
 
-        // If you use configure forms authentication in your Web.config and
-        // use <deny users="*"> to deny all requests, such as the following:
+        // If you use configure forms authentication in your Web.config and 
+        // use <deny users="*"> to deny all requests, such as the following: 
         //
         //  <location>
         //    <system.web>
@@ -36,10 +36,10 @@ namespace Ext.Net.MVC.Examples
         //    </system.web>
         //  </location>
         //
-        // Add the following Application_AuthenticateRequest event handler
-        // to your Global.asax.cs to exclude authenticating Ext.NET related
+        // Add the following Application_AuthenticateRequest event handler 
+        // to your Global.asax.cs to exclude authenticating Ext.NET related 
         // resource files (.js, .css, images).
-        //
+        // 
         //protected void Application_AuthenticateRequest(object sender, System.EventArgs e)
         //{
         //    // Skip authenticating all ext.axd embedded resources (.js, .css, images)
