b84fdf8ed576a0e72d8e4778566ce9130d9867a5
Fabrício Zimmerer Murta <fabricio@object.net>
2017-01-20 09:23:33 -0200

    Ports Window.Toast overview example from WebForms EE.
    The same example Window > Toast > Overview from WebForms Examples
    Explorer is ported to the MVC Examples Explorer.
    
    Related issue: extnet/Ext.NET#1414.

---
+++ src/Areas/Window_Toast/Controllers/OverviewController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Window_Toast.Controllers
+{
+    public class OverviewController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public DirectResult Button1_Click()
+        {
+            X.Toast("Toast shown!");
+            return this.Direct();
+        }
+
+        public DirectResult Button2_Click()
+        {
+            X.Toast("Toast with title at bottom shown!", "MyTitle", ToastAlign.Bottom);
+            return this.Direct();
+        }
+
+        public DirectResult Button3_Click()
+        {
+            object toastObj = new
+            {
+                html = "This toast was defined from code behind from an anonymous object!",
+                title = "Object toast",
+                align = JSON.EnumToString(ToastAlign.Right), // Map the enum to the client-side supported string.
+                anchor = "Button3",
+                stickWhileHover = true,
+                closable = true,
+                closeOnMouseDown = true,
+                autoClose = true,
+                autoCloseDelay = 2500
+            };
+
+            X.Toast(toastObj);
+
+            return this.Direct();
+        }
+
+    }
+}
