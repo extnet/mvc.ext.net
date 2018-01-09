bbe56568d936f4af694d0d35621f04258305ee68
Fabrício Zimmerer Murta <fabricio@object.net>
2018-01-09 18:56:05 -0200

    Upgrades Ext.NET Examples to Ext.NET 4.5.1.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("4.5.0")]
-[assembly: AssemblyFileVersion("4.5.0.0")]
-[assembly: AssemblyInformationalVersion("4.5.0")]
+[assembly: AssemblyVersion("4.5.1")]
+[assembly: AssemblyFileVersion("4.5.1.0")]
+[assembly: AssemblyInformationalVersion("4.5.1")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
-[assembly: AssemblyCopyright("Copyright (c) 2008-2017 Object.NET, Inc.")]
+[assembly: AssemblyCopyright("Copyright (c) 2008-2018 Object.NET, Inc.")]
 
 [assembly: AssemblyTitle("Ext.Net.MVC.Examples")]
 [assembly: AssemblyProduct("Ext.Net.MVC.Examples")]
