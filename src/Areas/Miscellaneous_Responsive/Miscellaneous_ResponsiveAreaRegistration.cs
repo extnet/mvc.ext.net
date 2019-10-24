4425b6fa245120bc3621cd4e3f4f8b18a52f00a8
Fabrício Zimmerer Murta <fabricio@object.net>
2019-10-24 04:53:15 -0300

    Ports responsive example from Ext.NET WebForms to MVC.

---
+++ src/Areas/Miscellaneous_Responsive/Miscellaneous_ResponsiveAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Miscellaneous_Responsive
+{
+    public class Miscellaneous_ResponsiveAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Miscellaneous_Responsive";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Miscellaneous_Responsive_default",
+                "Miscellaneous_Responsive/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
