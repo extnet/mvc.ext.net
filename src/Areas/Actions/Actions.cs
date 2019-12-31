63f5371538cfdb41eacb8de4d989b7e054b00e8c
Fabrício Zimmerer Murta <fabricio@object.net>
2019-12-31 00:47:36 -0300

    Adds samples for Actions - Simple and View.
    This is part of the porting process from WebForms examples into MVC.

---
+++ src/Areas/Actions/Actions.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Actions
+{
+    public class Actions : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Actions";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Actions_default",
+                "Actions/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/Areas/Actions/Controllers/SimpleController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Actions.Controllers
+{
+    public class SimpleController : Controller
+    {
+        public ActionResult Index() => View();
+    }
+}
