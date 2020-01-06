fe02c6f46bb09bf91f0e64e7bf7953f3b34c90c3
Fabrício Zimmerer Murta <fabricio@object.net>
2020-01-06 16:02:02 -0300

    Introduces Associations > Lazy Load example.
    This is an effort porting the example from WebForms, and uses partial
    views instead of custom controls for the lazy-loaded panels within it.
    
    The example currently does not load the order details, it is missing the
    _order() method in every client-side customer data entry.
    
    The store needed to force proxy as empty/null because from the partial
    view, the store is always coming with proxy type as 'memory' for some
    reason.

---
+++ src/Areas/Associations_HasMany/Controllers/Lazy_LoadController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Ext.Net.MVC.Examples.Areas.Associations_HasMany.Models;
+
+namespace Ext.Net.MVC.Examples.Areas.Associations_HasMany.Controllers
+{
+    [DirectController(AreaName = "Associations_HasMany", IDMode = DirectMethodProxyIDMode.None)]
+    public class Lazy_LoadController : Controller
+    {
+        public ActionResult Index() => View();
+
+        public ActionResult RenderCustomerGrid(string containerId) => new PartialViewResult
+        {
+            ContainerId = containerId,
+            RenderMode = RenderMode.AddTo,
+            ViewName = "CustomerGrid",
+            WrapByScriptTag = false
+        };
+
+        public ActionResult RenderLoggerPanel(string containerId) => new PartialViewResult
+        {
+            ContainerId = containerId,
+            RenderMode = RenderMode.AddTo,
+            ViewName = "Logger",
+            WrapByScriptTag = false
+        };
+
+        public ActionResult RenderModelsCluster(string containerId) => new PartialViewResult
+        {
+            ContainerId = containerId,
+            ViewName = "Models",
+            WrapByScriptTag = false
+        };
+
+        public ActionResult RenderOrderGrid(string containerId) => new PartialViewResult
+        {
+            ContainerId = containerId,
+            ViewName = "OrderGrid",
+            WrapByScriptTag = false
+        };
+
+        [DirectMethod]
+        public ActionResult GetCustomers(string action, Dictionary<string, object> extraParams)
+        {
+            var db = new Lazy_LoadModels();
+
+            if (extraParams.ContainsKey("id"))
+            {
+                int id = Convert.ToInt32(extraParams["id"]);
+                return this.Direct(db.Customers.Where(c => c.Id == id));
+            }
+
+            return this.Direct(db.Customers);
+        }
+
+        [DirectMethod]
+        public ActionResult GetOrders(string action, Dictionary<string, object> extraParams)
+        {
+            var db = new Lazy_LoadModels();
+
+            if (extraParams.ContainsKey("id"))
+            {
+                int id = Convert.ToInt32(extraParams["id"]);
+                return this.Direct(db.Orders.Where(o => o.Id == id));
+            }
+
+            int customerId = -1;
+
+            StoreRequestParameters prms = new StoreRequestParameters(extraParams);
+            DataFilter[] filter = prms.Filter;
+            if (filter.Length > 0 && filter[0].Property == "CustomerId")
+            {
+                customerId = Convert.ToInt32(filter[0].Value);
+            }
+
+            return this.Direct(db.Orders.Where(o => o.CustomerId == customerId));
+        }
+
+        [DirectMethod]
+        public ActionResult GetOrderItems(string action, Dictionary<string, object> extraParams)
+        {
+            var db = new Lazy_LoadModels();
+
+            if (extraParams.ContainsKey("id"))
+            {
+                int id = Convert.ToInt32(extraParams["id"]);
+                return this.Direct(db.OrderItems.Where(oi => oi.Id == id));
+            }
+
+            int orderId = -1;
+
+            StoreRequestParameters prms = new StoreRequestParameters(extraParams);
+            DataFilter[] filter = prms.Filter;
+            if (filter.Length > 0 && filter[0].Property == "OrderId")
+            {
+                orderId = Convert.ToInt32(filter[0].Value);
+            }
+
+            return this.Direct(db.OrderItems.Where(oi => oi.OrderId == orderId));
+        }
+    }
+}
