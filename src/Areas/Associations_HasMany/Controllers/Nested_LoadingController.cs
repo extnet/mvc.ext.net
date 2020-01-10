9383182b79fe22bd1210bc621dad175e4e1b8cbc
Fabrício Zimmerer Murta <fabricio@object.net>
2020-01-10 18:28:54 -0300

    Adds the Associations-HasMany > Nested Loading example.

---
+++ src/Areas/Associations_HasMany/Controllers/Nested_LoadingController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Associations_HasMany.Controllers
+{
+    public class Nested_LoadingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
