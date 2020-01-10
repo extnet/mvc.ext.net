135354f503b4fed32c469e53d88a2c26f7a9b185
Fabrício Zimmerer Murta <fabricio@object.net>
2020-01-10 20:24:44 -0300

    Adds the Associations HasOne > Simple Lazy Load example.

---
+++ src/Areas/Associations_HasOne/Controllers/Simple_Lazy_LoadController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Ext.Net.MVC.Examples.Areas.Associations_HasOne.Models;
+
+namespace Ext.Net.MVC.Examples.Areas.Associations_HasOne.Controllers
+{
+    [DirectController(AreaName = "Associations_HasOne", GenerateProxyForOtherAreas = false, GenerateProxyForOtherControllers = false, IDMode = DirectMethodProxyIDMode.None)]
+    public class Simple_Lazy_LoadController : Controller
+    {
+        class query
+        {
+            public int id { get; set; }
+        }
+
+        [DirectMethod(ShowMask = true, CustomTarget = "#{AddressPanel}")]
+        public ActionResult GetAddress(string action, string paramsJson)
+        {
+            var extraParams = JsonConvert.DeserializeObject<query>(paramsJson);
+            return this.Direct(new List<Address>()
+            {
+                new Address(1, "1", "Cross Street 11/1", "Big City", "123456" ),
+                new Address(2, "2", "Cross Street 12/2", "Big City", "654321" ),
+                new Address(3, "3", "Cross Street 13/3", "Big City", "321654" )
+            }.Where(match => match.Id == extraParams.id));
+        }
+
+        public ActionResult Index()
+        {
+            return View(new List<Simple_Lazy_Load.Person>
+            {
+                new Simple_Lazy_Load.Person(1, "John Smith", 1 ),
+                new Simple_Lazy_Load.Person(2, "Jane Brown", 2 ),
+                new Simple_Lazy_Load.Person(3, "Kevin Jones", 3 )
+            });
+        }
+    }
+}
---
+++ src/Areas/Associations_HasOne/Models/Simple_Lazy_LoadModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Ext.Net.MVC.Examples.Areas.Associations_HasOne.Models
+{
+    public class Simple_Lazy_Load
+    {
+        public class Person
+        {
+            public Person(int id, string name, int addressId)
+            {
+                this.Id = id;
+                this.Name = name;
+                this.AddressId = addressId;
+            }
+
+            public int Id
+            {
+                get;
+                private set;
+            }
+
+            public string Name
+            {
+                get;
+                private set;
+            }
+
+            public int AddressId
+            {
+                get;
+                private set;
+            }
+        }
+    }
+
+}
