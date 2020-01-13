ce957222b6479fc899c1e80e072c81628b4bd20b
Fabrício Zimmerer Murta <fabricio@object.net>
2020-01-13 17:39:16 -0300

    Adds the Buttons > Basic > Loading State example.

---
+++ src/Areas/Buttons_Basic/Controllers/LoadingStateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Buttons_Basic.Controllers
+{
+    public class LoadingStateController : Controller
+    {
+        public ActionResult Index() => View();
+
+        public ActionResult Button1_Click()
+        {
+            System.Threading.Thread.Sleep(2000);
+
+            X.Msg.Alert("Server Time", DateTime.Now.ToLongTimeString()).Show();
+            return this.Direct();
+        }
+    }
+}
