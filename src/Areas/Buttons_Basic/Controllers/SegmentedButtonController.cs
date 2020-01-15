41598f1057f7df4c2cd9904139247b3c9e0f1379
Fabrício Zimmerer Murta <fabricio@object.net>
2020-01-15 12:41:00 -0300

    Adds the Buttons > Basic > Segmented Button example.

---
+++ src/Areas/Buttons_Basic/Controllers/SegmentedButtonController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Buttons_Basic.Controllers
+{
+    public class SegmentedButtonController : Controller
+    {
+        public ActionResult Index() => View();
+    }
+}
