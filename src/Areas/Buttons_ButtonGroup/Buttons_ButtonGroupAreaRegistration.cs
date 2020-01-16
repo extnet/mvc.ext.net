5d86dd8353bd3344df9e826c28576dc91eed9366
Fabrício Zimmerer Murta <fabricio@object.net>
2020-01-15 23:46:29 -0300

    Adds the Buttons > ButtonGroup > Overview example.

---
+++ src/Areas/Buttons_ButtonGroup/Buttons_ButtonGroupAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Buttons_ButtonGroup
+{
+    public class Buttons_ButtonGroupAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Buttons_ButtonGroup";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Buttons_ButtonGroup_default",
+                "Buttons_ButtonGroup/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/Areas/Buttons_ButtonGroup/Controllers/OverviewController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Buttons_ButtonGroup.Controllers
+{
+    public class OverviewController : Controller
+    {
+        public ActionResult Index() => View();
+    }
+}
