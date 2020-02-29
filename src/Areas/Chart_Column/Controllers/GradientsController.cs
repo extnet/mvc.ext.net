13b0f906f5ce6baae5e3990e29cd84144239a705
Fabrício Zimmerer Murta <fabricio@object.net>
2020-02-29 17:53:29 -0300

    Adds the Charts > Column > Gradients example.
    This is simply a port from WebForms version.

---
+++ src/Areas/Chart_Column/Controllers/GradientsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Chart_Column.Controllers
+{
+    public class GradientsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(ChartModel.GenerateData(5, 0));
+        }
+
+        public StoreResult GetData()
+        {
+            return new StoreResult(ChartModel.GenerateData(5, 0));
+        }
+    }
+}
