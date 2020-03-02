892e5d4d6ead452e0e7eeb94540bc319bed164e2
Fabrício Zimmerer Murta <fabricio@object.net>
2020-03-02 16:09:09 -0300

    Adds the Charts > Column > MultiAxis example.
    Similarly to the Charts > Column > Limit example recently added, this
    needed a special treatment using the Razor functions concept to
    simplify adding gradient stops to the rainbow gradient featured.
    
    It also could use an overload for BarSeries.Substyle config for more
    seamless specification of the setting.

---
+++ src/Areas/Chart_Column/Controllers/MultiAxisController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Chart_Column.Controllers
+{
+    public class MultiAxisController : Controller
+    {
+        public class Temperature
+        {
+            public string Month
+            {
+                get;
+                set;
+            }
+
+            public double High
+            {
+                get;
+                set;
+            }
+
+            public double Low
+            {
+                get;
+                set;
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View(new List<Temperature>
+            {
+                new Temperature { Month = "Jan", High = 14.7, Low = 5.6  },
+                new Temperature { Month = "Feb", High = 16.5, Low = 6.6  },
+                new Temperature { Month = "Mar", High = 18.6, Low = 7.3  },
+                new Temperature { Month = "Apr", High = 20.8, Low = 8.1  },
+                new Temperature { Month = "May", High = 23.3, Low = 9.9  },
+                new Temperature { Month = "Jun", High = 26.2, Low = 11.9 },
+                new Temperature { Month = "Jul", High = 27.7, Low = 13.3 },
+                new Temperature { Month = "Aug", High = 27.6, Low = 13.2 },
+                new Temperature { Month = "Sep", High = 26.4, Low = 12.1 },
+                new Temperature { Month = "Oct", High = 23.6, Low = 9.9  },
+                new Temperature { Month = "Nov", High = 17  , Low = 6.8  },
+                new Temperature { Month = "Dec", High = 14.7, Low = 5.8  }
+            });
+        }
+    }
+}
