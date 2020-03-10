5f865e13b2bf3df40e2e150019a5b963bb5ba971
Fabrício Zimmerer Murta <fabricio@object.net>
2020-03-10 00:32:50 -0300

    Adds the Charts > Combination > Binding Tabs example.
    This is simply a port from WebForms corresponding example. The ViewModel
    in here bound using MVC's ViewData that is synced between the controller
    and view to properly fill the tab panel's ViewModel. An usual '@model'
    would probably also work just fine, but this shows an alternative
    approach to binding initial data to Ext.NET MVC components.

---
+++ src/Areas/Chart_Combination/Controllers/Binding_TabsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+
+namespace Ext.Net.MVC.Examples.Areas.Chart_Combination.Controllers
+{
+    public class Binding_TabsController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData.Model =  new
+            {
+                stores = new
+                {
+                    priceData = new
+                    {
+                        fields = new string[] { "month", "price" },
+                        data = new object[]
+                        {
+                            new { month = "Jan", price = 28 },
+                            new { month = "Feb", price = 25 },
+                            new { month = "Mar", price = 21 },
+                            new { month = "Apr", price = 18 },
+                            new { month = "May", price = 18 },
+                            new { month = "Jun", price = 17 },
+                            new { month = "Jul", price = 16 },
+                            new { month = "Aug", price = 16 },
+                            new { month = "Sep", price = 16 },
+                            new { month = "Oct", price = 16 },
+                            new { month = "Nov", price = 15 },
+                            new { month = "Dec", price = 15 }
+                        }
+                    }
+                }
+            };
+
+            return View();
+        }
+    }
+}
